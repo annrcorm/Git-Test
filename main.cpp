d0917bbf44605a0549541d524188ba514ae9e443
Annabelle Cormia <annrcorm@ut.utm.edu>
2016-03-31 21:17:35 -0500

    Second commit: adding new main.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main(){
 
+  int n;
+  cout << "Enter an integer for n. ";
+  cin >> n;
+  sum(n);
+  cout << "\nCurrent value: " << n << endl;
+
   cout << "Hello world!" << endl;
+
+}
+
+int sum(int n){
+
+  int sum;
+
+  for (int i = 1; i < n; i++){
+    sum+=i;
+  }
+
+  return sum;
 
 }
